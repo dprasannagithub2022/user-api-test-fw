2da6de607e823000b0124e81d2a03a684c5e419a
Padma924 <padma924@gmail.com>
2024-04-27 19:58:04 -0700

    Added string Reverse Program

---
+++ src/main/java/com/apex/samples/test/Padma/StringReverseProgram.java
@@ -0,0 +1,18 @@
+package com.apex.samples.test.Padma;
+
+public class StringReverseProgram {
+
+	public static void main(String[] args) {
+			
+			String str = "welcome";
+			String revStr = "";
+			int len = str.length();
+			for(int i=len-1;i>=0;i--) {
+				char c = str.charAt(i);
+				revStr+= c;
+			}
+	        System.out.println("Reverse String is:"+ revStr);
+		}
+}
+
+
